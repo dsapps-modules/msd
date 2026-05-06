a24f969e75eb7173c38e5eb7f737a17fe5605f68
Isadora <dorinhap285@gmail.com>
2026-05-06 15:49:41 -0300

    First commit -  parte 26

---
+++ admin/build/server/app/[locale]/api/image-proxy/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/[locale]/api/image-proxy/route.js")
+R.c("server/chunks/[root-of-the-server]__04lojtl._.js")
+R.c("server/chunks/[root-of-the-server]__0op9af~._.js")
+R.c("server/chunks/_next-internal_server_app_[locale]_api_image-proxy_route_actions_09cvjp2.js")
+R.m(277312)
+module.exports=R.m(277312).exports
---
+++ admin/build/server/app/[locale]/api/pusher/route.js
@@ -0,0 +1,8 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/[locale]/api/pusher/route.js")
+R.c("server/chunks/[root-of-the-server]__0ami64j._.js")
+R.c("server/chunks/node_modules_next_04~_e52._.js")
+R.c("server/chunks/[root-of-the-server]__0op9af~._.js")
+R.c("server/chunks/[root-of-the-server]__01yh19~._.js")
+R.c("server/chunks/_next-internal_server_app_[locale]_api_pusher_route_actions_11-rfyb.js")
+R.m(492017)
+module.exports=R.m(492017).exports
