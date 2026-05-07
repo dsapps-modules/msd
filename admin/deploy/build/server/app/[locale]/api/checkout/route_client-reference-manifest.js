49f72f73d999543bfd09dc61eed90e9ca330fd68
Isadora <dorinhap285@gmail.com>
2026-05-07 08:41:41 -0300

    First commit -  parte 42

---
+++ admin/deploy/build/server/app/[locale]/api/checkout/route_client-reference-manifest.js
@@ -0,0 +1,3 @@
+globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
+globalThis.__RSC_MANIFEST["/[locale]/api/checkout/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}};
+
